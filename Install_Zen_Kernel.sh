88007c5a2dafc58c02410e32227d6096fb060501
4rge <93495092+4rge@users.noreply.github.com>
2025-01-30 15:15:40 -0800

    Create Install_Zen_Kernel.sh

---
+++ Install_Zen_Kernel.sh
@@ -0,0 +1,5 @@
+sudo pacman-key --refresh
+sudo pacman -Sy archlinux-keyring
+sudo pacman -Syyu
+wget https://ftp5.gwdg.de/pub/linux/archlinux/extra/os/x86_64/linux-zen-6.12.6.zen1-1-x86_64.pkg.tar.zst && sudo pacman -U linux-zen-6.2.12.zen1-1-x86_64.pkg.tar.zst
+sudo pacman -U linux-zen-6.12.6.zen1-1-x86_64.pkg.tar.zst
